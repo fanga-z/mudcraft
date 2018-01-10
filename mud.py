5d2397365678c9d9bbe5a1bc84f87459223f651b
fanga-z <fangl@welovecode.cn>
2018-01-10 22:11:58 +0800

    增加了打东西
    还差物品掉落

--- mud.py
+++ mud.py
@@ -43,8 +43,46 @@ class Role:
     def add_exp(self, exp):
         self.exp += exp
         print ("exp +", str(exp))
-    def act(self, action, obj):
-        print (obj.act(action, self))
+    def _punch(self, sel, target):
+        
+        if not sel in target.punch_options.keys():
+            return
+        
+        value = target.punch_options[sel]
+         
+        punch_target = target.get_object(value)
+        punch_target.react("punch", self)
+        if punch_target.hp <= 0:
+            target.remove_object(value)
+            del target.punch_options[sel]
+    def _go(self, sel, target):
+        if not sel in target.go_options.keys():
+            return
+        result = target.go_options_file[sel]
+        time.sleep(1)
+        Director.nextScene(target, result)
+    def act(self, action, target):
+        if action.startswith("go"):
+            arg = action.split(" ")
+            if len(arg) == 2:
+                self._go(arg[1], target)
+            else:
+                print (target.go_options)
+                sel = input("请选择:"+ str(list(target.go_options.keys())))
+                self._go(sel, target)
+        elif action.startswith("punch"):
+            arg = action.split(" ")
+            
+            if len(arg) == 2:
+                self._punch(arg[1], target)
+            else:    
+                print (target.punch_options)
+                sel = input("请选择:"+ str(list(target.punch_options.keys())))
+                self._punch(sel, target)
+        else:
+            print (target.react(action, self))
+   
+        
     def bye(self):
         f = open("role.txt", "w" , encoding="UTF-8")
         f.write(str(self.exp) + "\n")
@@ -63,60 +101,93 @@ class Scene:
         
         txt = f.readline().strip()
         actions = {}
-        while not txt.startswith("go"):
-            kv = txt.split(":")
-            actions[kv[0].strip()] = kv[1].strip()
+        while not txt == "":
+             
+            if txt.startswith("go"):
+                txt = txt[3:]
+                opt = txt.split(",")
+                
+                go_options = {}
+                go_options_file = {}
+                for i in opt:
+                    kv = i.split("#")
+                    go_options[kv[0].strip()] = kv[1].strip()
+                    go_options_file[kv[0].strip()] = kv[2].strip()
+                self.go_options = go_options
+                self.go_options_file = go_options_file    
+            elif txt.startswith("punch"):
+                txt = txt[6:]
+                opt = txt.split(",")
+                punch_options = {}
+                for i in opt:
+                    kv = i.split("#")
+                    punch_options[kv[0].strip()] = kv[1].strip()
+                    
+                self.punch_options = punch_options
+            else:
+                kv = txt.split(":")
+                actions[kv[0].strip()] = kv[1].strip()
             txt = f.readline().strip()
 
+         
         self.actions = actions
-        
-        
-        txt = txt[3:]
-        opt = txt.split(",")
-        
-        go_options = {}
-        go_options_file = {}
-        for i in opt:
-            kv = i.split("#")
-            go_options[kv[0].strip()] = kv[1].strip()
-            go_options_file[kv[0].strip()] = kv[2].strip()
-        self.go_options = go_options
-        self.go_options_file = go_options_file
+        #场景里的物体 使用的时候自动创建
+        self.objects = {}
         f.close()
         
-        
+    def get_object(self, value):
+        if value in self.objects.keys():
+            return self.objects[value]
+        else:
+            obj = Object(value)
+            self.objects[value] = obj
+            return obj
+    def remove_object(self, value):
+        if value in self.objects.keys():
+            del self.objects[value]
+            
     def file(self):
         return self.fileTxt
     def where(self):
         return self.place    
     def left(self):
         pass
-    def act(self, action, who):
+    def react(self, action, who):
         if action in self.actions.keys():
             who.add_exp(random.randint(1,10))
             return self.actions[action]
         else:
             return "please don't " + action
 
+class Object:
+    def __init__(self, name):
+        self.name = name
+        self.hp = random.randint(3,6)
+        self.reward = "武林秘籍"
+    def react(self, action, who):
+        if action == "punch":
+            who.add_exp(random.randint(1,10))
+            self.hp -= 1
+            if self.hp == 0:
+                print ("你赢了 得到" + self.reward)
+            else:
+                print ("你打了"+self.name + "一下 还得打"+str(self.hp)+"下")
+        
+        
 current = None
 you = Role()
 
 running = True
 while running:
     
-    action = input ("")
+    action = input (">>")
+    action = action.strip()
     if action == "hp":
         print (you.hp)
     elif action == "exp":
         print (you.exp)
     elif action == "when":
         print ("[一月]已经是正午了，太阳躲在雾霾的后面，没有一点暖和的意思")  
-    elif action == "go":
-        print (current.go_options)
-        sel = input("请选择:"+ str(list(current.go_options.keys())))
-        result = current.go_options_file[sel]
-        time.sleep(1)
-        Director.nextScene(current, result)  
     elif action == "bye":
         you.bye()
         running = False
