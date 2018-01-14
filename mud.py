dcd3e4cb25349286a93eed7f1aa97eed163b243b
fangl <fangl@welovecode.cn>
2018-01-14 18:39:42 +0800

    把天气阴晴数据提出来了
    把命令也改成了today

--- mud.py
+++ mud.py
@@ -16,10 +16,12 @@ while running:
         you.bag.view(you)
     elif action == "exp":
         print (you.exp)
-    elif action == "when":
+    elif action == "today":
         info = weather.get_weather()
         if info != None:
-            print (info['city'] + " PM2.5 " + str(info['data']['pm25']) + " 空气质量 " + info['data']['quality'] + " 温度 " + str(info['data']['wendu']) )
+            print (info['city'], end=" ")
+            print (info['data']['forecast'][0]['date'], info['data']['forecast'][0]['type'], end=" ")
+            print (" PM2.5=" + str(info['data']['pm25']) ,  info['data']['quality'])
     elif action == "bye":
         you.bye()
         running = False
